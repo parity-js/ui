a11b67bd99f56803a26d173f2625f58e29acaf97
Amaury Martiny <amaury.martiny@protonmail.com>
2018-07-10 18:01:14 +0200

    Remove unused var

--- src/StatusIndicator/store.js
+++ src/StatusIndicator/store.js
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import { observable, computed } from 'mobx';
+import { computed } from 'mobx';
 import stores from '@parity/mobx';
 
 export const STATUS_BAD = 'bad';
